dfe1e2ae7f7725f13c745e8147c1042587ab8649
Pratik Bhuvad <110712654+Pratik-Bhuvad@users.noreply.github.com>
2025-06-10 20:03:37 +0530

    Add authentication middleware for token verification

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../config/env')
+
+const authMiddleware = async (req, res, next) => {
+    const token = req.cookie.Verba || req.headers.authorization?.split(' ')[1];
+
+    if (!token) return res.status(401).json({ message: 'Unauthorized' });
+
+    try {
+        jwt.verify(token, JWT_SECRET, (err, decoded) => {
+            if (err) return res.status(403).json({ message: 'Forbidden' });
+            req.user = decoded
+            next()
+        })
+    } catch (error) {
+        console.error('JWT verification error:- ', error.message);
+        throw error
+    }
+}
+
+module.exports = authMiddleware
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,6 +2,7 @@ const express = require('express')
 const rateLimit = require('express-rate-limit')
 const { RegisterValidations, LoginValidations } = require('../validators/authValidators')
 const { RegisterUser, Login } = require('../controllers/authController')
+const authMiddleware = require('../middleware/authMiddleware')
 const router = express.Router()
 
 const authLimiter = rateLimit({
